228f85afb5d9eb2792e63b7f2d85eabb7455a71b
Richard <lapislacrima@gmail.com>
2016-12-06 11:28:35 -0800

    Fixed issue where shows_name was being updated with ids instead of name \n Escaped text for injectShow so that the names would be parsed correctly

--- database_api/writeUserRating.js
+++ database_api/writeUserRating.js
@@ -1,29 +1,48 @@
 "use strict";
 
 const settings = require('./knex_settings');
-
 const knex = require('knex')(settings);
-
 const Promise = require('bluebird');
-let currentTime = (new Date (Date.now())).toISOString().slice(0,10);
+const sample_input = require('./1653.json');
 
 function writeAllRatings(input_json){
-  const nid = input_json['user_id'];
+  const user_nid = input_json['user_id'];
   const user_name = input_json['user_name'];
-  // let currentTime = (new Date (Date.now())).toISOString().slice(0,10);
   return knex.transaction(function(transact){
-    injectUser(user_name, nid, currentTime, transact)
+    injectUser(user_name, user_nid, transact)
     .then(() => {
-          show_promises = [];
-          rank_promises = [];
-    }
-      )
+      let show_promises = [];
+      let rank_promises = [];
+      for (let show_nid in input_json['user_ratings']){
+        show_promises.push(
+          injectShow(input_json['user_ratings'][show_nid]['series_title'], show_nid, transact)
+          );
+        rank_promises.push(
+          injectRating(user_nid, show_nid, input_json['user_ratings'][show_nid]['my_score'], input_json['user_ratings'][show_nid]['my_last_updated'], transact)
+          );
+      }
+      return (Promise.all(show_promises))
+        .then(Promise.all(rank_promises))
+        .catch((err) => {
+          throw err
+        })
+    })
+    .then(transact.commit)
+    .catch((err) => {
+      transact.rollback;
+      console.log('transaction failed with \n', err)
+    })
+  })
+  .then(() => {
+    console.log(`Rating information for user ${user_name} with id ${user_nid} added`)
+  })
+  .catch((err) => {
+    console.log(`Rating information for user ${user_name} with id ${user_nid} failed with error ${err}`)
   })
 }
 
 function injectUser(user_name, nid, transact){
   let currentTime = Math.floor(Date.now()/1000);
-  currentTime = '1174107141'
   return knex.raw(
       `
         INSERT INTO users (nid, name, last_queried)
@@ -38,10 +57,10 @@ function injectShow(show_name, nid, transact){
   return knex.raw(
       `
         INSERT INTO shows (nid, name)
-        VALUES (${nid}, '${show_name}') ON CONFLICT (nid) DO
+        VALUES (?, ?) ON CONFLICT (nid) DO
         UPDATE
-        SET nid = ${nid}
-      `
+        SET nid = ?
+      `, [nid, show_name, nid]
     )
 }
 
@@ -57,6 +76,8 @@ function injectRating(user_nid, show_nid, rating, last_rated){
 }
 ;
 
-injectUser('sample', '21', currentTime).then(console.log('complete'))
-injectShow('Bloop', '548').then(console.log('Show added'))
-injectRating('21', '548', 2, "1174107141").then(console.log('Rating added'))
+// injectUser('sample', '21').then(console.log('complete'))
+// injectShow('Bloop', '548').then(console.log('Show added'))
+// injectRating('21', '548', 2, "1174107141").then(console.log('Rating added'))
+writeAllRatings(sample_input);
+// console.log(escape("dfa's"))
