abd4871a3c5779eee7879f00a4897209a7011d20
Richard <lapislacrima@gmail.com>
2016-12-19 15:54:49 -0800

    load path from command line arguments
    path is now loaded from command line arguments so that relative paths to the rds assets can be avoided

--- recommender/recommend.R
+++ recommender/recommend.R
@@ -3,13 +3,16 @@ library('jsonlite')
 
 args = commandArgs(trailingOnly = TRUE)
 
+# extract path
+current_dir = args[1]
+  
 # convert JSON arguments to a list
-user_ratings = (fromJSON(args))
+user_ratings = (fromJSON(args[2]))
 
 # load assets
-svd_inv = readRDS("svd_inv.rds")
-svd_mod = readRDS("svd.rds")
-show_means = readRDS("show_means.rds")
+svd_inv = readRDS(file.path(current_dir, "svd_inv.rds"))
+svd_mod = readRDS(file.path(current_dir, "svd.rds"))
+show_means = readRDS(file.path(current_dir, "show_means.rds"))
 
 # impute scores
 user_input = show_means
@@ -27,3 +30,5 @@ t = as.numeric(user_input) %*% svd_inv$inv_v %*% svd_inv$inv_d
 output_recommendation = t %*% diag(svd_mod$d) %*% t(svd_mod$v)
 names(output_recommendation) = names(show_means)
 write(names(sort(output_recommendation, decreasing=TRUE)), stdout())
+write(current_dir, stderr())
+
