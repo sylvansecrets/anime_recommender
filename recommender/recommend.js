1d68a1d86551e8af7a2971081923f55d86b4d47d
Richard <lapislacrima@gmail.com>
2016-12-19 15:57:08 -0800

    call recommend.R to retreive ordered ratings
    uses child-process-promise to execute recommend.R and parses the returned list of show_nids

--- recommender/recommend.js
+++ recommender/recommend.js
@@ -1,17 +1,22 @@
+const path = require('path')
+
 const rscriptHeader = 'Rscript --vanilla';
-const rscriptTarget = 'recommend.R';
+const rscriptTarget = path.join(__dirname, 'recommend.R');
+const rscriptLocation = __dirname;
 
 const exec = require('child-process-promise').exec
 
 function fetchRecommendations(ratingJSON){
-  const scriptString = `${rscriptHeader} ${rscriptTarget} '${JSON.stringify(ratingJSON)}'`
+  const scriptString = `${rscriptHeader} ${rscriptTarget}  ${rscriptLocation} '${JSON.stringify(ratingJSON)}'`
+  console.log(scriptString)
   return exec(scriptString)
 }
 
 function cleanRecommendations(ratingJSON){
   return fetchRecommendations(ratingJSON)
     .then(result => {
-      console.log(result.stdout.split('\n').map(Number).filter(Boolean))
+      console.log(result.stderr)
+      return(result.stdout.split('\n').map(Number).filter(Boolean))
     })
 }
 
