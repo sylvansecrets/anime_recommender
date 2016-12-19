0d2891498c63e836822622befddccb1ae91f9878
Richard <lapislacrima@gmail.com>
2016-12-19 01:04:22 -0800

    print recommendation given user ratings
    reconstructs user ratings by multiplying by inverse of loadings and inverse of the right matrix.
    missing data imputed by show means

---
+++ recommender/recommend.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+library('jsonlite')
+library('MASS')
+args = commandArgs(trailingOnly = TRUE)
+
+# convert JSON arguments to a list
+user_ratings = (fromJSON(args))
+
+# load assets
+svd_mod = readRDS("svd.rds")
+show_means = readRDS("show_means.rds")
+
+# impute scores
+user_input = show_means
+
+# replace imputed scores with real ratings where valid
+for (i in seq_len(length(user_ratings))){
+  name_ind = match(names(user_ratings)[i], names(user_input))
+  user_input[name_ind] = user_ratings[i]
+}
+
+# reconstruct user preferences in k-space
+t = as.numeric(user_input) %*% ginv(t(svd_mod$v)) %*% ginv(diag(svd_mod$d))
+
+# calculate and print the recommendations
+output_recommendation = t %*% diag(svd_mod$d) %*% t(svd_mod$v)
+names(output_recommendation) = names(show_means)
+print(names(sort(output_recommendation, decreasing=TRUE)))
